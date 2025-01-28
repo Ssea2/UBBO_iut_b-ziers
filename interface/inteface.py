0349eb46aafb9d3a85447c2ab4a337327301216a
Mustangos <rayan.ouraghi@hotmail.com>
2025-01-28 16:42:36 +0100

    ajout interface

---
+++ interface/inteface.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route pour l'accueil
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route pour la page de présentation
+@app.route('/action')
+def action():
+    return render_template('presentation.html')
+
+# Route pour la page "qsj"
+@app.route('/who')
+def who():
+    return render_template('qsj.html')
+
+# Route pour la page du jeu
+@app.route('/game')
+def game():
+    return render_template('jeu.html')
+
+if __name__ == '__main__':
+    app.run(host='10.216.0.108', port=5000, debug=True, threaded=False)
