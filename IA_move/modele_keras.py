91d49bb7888431c4ce588e93f21e3fe15fdc0f1b
Ssea2 <ssea2.heal@gmail.com>
2025-01-30 12:15:25 +0100

    ajout du num de la version de TF

--- IA_move/modele_keras.py
+++ IA_move/modele_keras.py
@@ -1,4 +1,4 @@
-from keras.models import load_model # TensorFlow is required for Keras to work
+from keras.models import load_model # TensorFlow=2.11.1 is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
 import cv2
