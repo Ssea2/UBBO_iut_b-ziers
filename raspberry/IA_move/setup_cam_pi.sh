6fd577e7f3ce95cf13955ca0d0234589c8b42260
Ssea2 <ssea2.heal@gmail.com>
2025-01-30 15:39:12 +0100

    t

--- raspberry/IA_move/setup_cam_pi.sh
+++ raspberry/IA_move/setup_cam_pi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-sudo apt update
-sudo apt install adb
-sudo apt install v4l2loopback-utils
-sudo apt install v4l-utils
+apt update
+apt install adb
+apt install v4l2loopback-utils
+apt install v4l-utils
 
 adb forward tcp:4747 tcp:4747
-sudo modprobe v4l2loopback devices=1 video_nr=0 card_label="DroidCam"
+modprobe v4l2loopback devices=1 video_nr=0 card_label="DroidCam"
