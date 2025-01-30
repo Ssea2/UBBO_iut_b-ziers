b52122c4a2475279e1d5f9fe149eb249c57f8268
Ssea2 <ssea2.heal@gmail.com>
2025-01-30 16:30:35 +0100

    ajout fonction forward,backward,left,right

--- raspberry/move_rbot_arduino.py
+++ raspberry/move_rbot_arduino.py
@@ -4,12 +4,18 @@ import time
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 time.sleep(2)
 
-def envoie_donnee(message):
-    arduino.write((message+ '\n').encode())
+def envoie_donnee(mouvement):
+    mouvements= {"Stop":"1", "Go_Forward":"2", "Go_Backward":"3","Go_Turnright":"4","Go_Turnleft":"5", "Up_tab":"u","Down_Tab":"d"}
+    if mouvement in mouvements: 
+       message= str(mouvements[mouvement])
+       arduino.write((message+ '\n').encode())
+       print(f"Commande:{mouvement}({message})")
+       
     reponse= arduino.readline().decode().strip()
     if reponse:
-        print("Reponse de l'arduino:",reponse)
-    
+       print("Reponse de l'arduino:",reponse)
+
+
 def robot_dance():
     Up=["Up_tab" for i in range(20)]
     Down=["Down_Tab" for i in range(20)]
@@ -28,3 +34,24 @@ def robot_dance():
     for i in range(len(mouvement)):
         envoie_donnee(mouvement[i])
         time.sleep(temps_move[i])
+
+def forward():
+    envoie_donnee("Go_Forward")
+
+def backward():
+    envoie_donnee("Go_Backward")
+
+def turn_right():
+    envoie_donnee("Go_Turnright")
+
+def turn_left():
+    envoie_donnee("Go_Turnleft")
+
+def up_screen():
+    envoie_donnee("Up_tab")
+
+def down_screen():
+    envoie_donnee("Down_Tab")
+
+def stop():
+    envoie_donnee("Stop")
